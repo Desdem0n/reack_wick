2dfecf63844c052a1ead3c51d091742163fcd3e3
Michael Milke <desmilke@gmail.com>
2020-01-06 11:32:54 +0100

    to push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+function Books() {
+    return (
+        
+         <section className="books">
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          
+         </section>
+        
+    );
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Book = () => {
+    return (
+        <article className="book">
+          <CoverImage />
+          <Title />
+          <Author />
+        </article>
+    );
+};
+const CoverImage = () => (
+<img style={ima}
+width="200"
+src="https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg"
+ alt="michelle"/>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const authorStyle = {
+    letterSpacing: "10px",
+    color: "lightgrey"
+};
+
+const ima = {
+    margin: "5px",
+    textalign: "center"
+};
+
+const Title = () => 
+    <h2>Becoming Michelle Obama</h2>
+const Author = () => 
+    <p style={authorStyle}>by Jerry Higgins</p>;
+
+ReactDOM.render(<Books />, document.getElementById('root'));
